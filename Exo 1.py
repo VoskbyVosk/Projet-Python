0287cc013f52808307c9bd79bce3d27fc0616f4b
Lennytournier <ltournier@gaming.tech>
2022-11-08 12:03:47 +0100

    exo

--- Exo 1.py
+++ Exo 1.py
@@ -1,5 +1,18 @@
 DEBUT
 
-ECRIRE "BONJOUR MONDE"
+#ECRIRE "BONJOUR MONDE"
+#PRINT "HELLO WORLD"
+#print ("Hello World")
+
+assertionUn = (( (365 * 3) / (24 - (16 - 8) )) * (230) > 12000) == (10 * 1250 < 12000)
+
+def1 = 15 740.625 > 12 000 vaut True
+
+def2 = 12 500 < 12 000 vaut False
+
+def3 = 15 740.625 == 12 500 vaut False
+
+assertionDeux = ( (365 * 3) / (24 - (16 - 8) ) * (230) > 12000) == (10 * 1250 < 12000)
+
 
 FIN

87d1fdbff35820871468f6ba3120f27871bce460
Lennytournier <ltournier@gaming.tech>
2022-11-08 14:33:19 +0100

    avancé exo

--- Exercices.py
+++ Exercices.py
@@ -1,3 +1,5 @@
+from random import *
+
 def calculSalaireParSeconde(salaireHoraire, heureParJourOuvrable, jourOuvrable):
     #Assigner a salaireAnnuel, le nombre d'heure travaillées en un an
     salaireAnnuel = salaireHoraire * heureParJourOuvrable * jourOuvrable
@@ -15,4 +17,34 @@ def withdrawFees(total, percent):
 
 def calculSalaireNet(salaireBrut, coeff):
     #Calculer et Retourner le salaire Net a partir du salaire Brut
-    return withdrawFees(salaireBrut; coeff)
+    #return withdrawFees(salaireBrut, coeff)
+
+    #Si j'occupe un poste de la fonction publique
+    if public :
+        #alors je retourne le salaire brut - 15% de taxes
+        return withdrawFees(salaireBrut, 15)
+    #Sinon ? C'est que je suis travailleur dans le secteur privé, alors je retourne le salaire brut - 23% de douille à l'ancienne 
+    else:
+        return withdrawFees(salaireBrut, 23)
+
+
+def divide(x,y):
+    #Si y est égal a 0, alors la division est impossible
+    if y == 0:
+        print("Bah alors ?")
+        return None
+    #Sinon
+    else:
+        return x / y
+
+
+
+def input():
+    i = input("Veuillez saisir un caractère")
+
+
+def mini_jeu():
+    x = 0
+    while i != g:
+        x = x + 1
+    return "Valeur trouvé en " +x+ "essai(s)"

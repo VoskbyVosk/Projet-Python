d6323ac05ffac2de7308379e1b5fc6937c9a9073
Lennytournier <ltournier@gaming.tech>
2022-11-10 14:11:09 +0100

    Ajout rendu

---
+++ Rendu Final.py
@@ -0,0 +1,24 @@
+#Importer la bibliothèque random
+
+#Définir fonction PFC avec x l'option de jeu
+  #Créer liste avec les trois options de jeu
+  #Attribuer une option aléatoire à l'ordinateur
+  #Si le joueur et l'ordinateur on choisis la même option
+    #Alors afficher Egalité
+  #Sinon si le joueur choisi pierre
+    #Si l'ordinateur à choisi papier
+      #Alors afficher Perdu
+    #Sinon
+      #Afficher Gagné
+  #Sinon si le joueir choisi papier
+    #Si l'ordinateur choisi ciseaux
+      #Afficher Perdu
+    #Sinon
+      #Afficher Gagné
+  #Sinon si le joueur choisi ciseaux
+    #Si l'ordinateur choisi pierre
+      #Afficher Perdu
+    #Sinon
+      #Afficher Gagné
+  #Sinon
+    #Afficher erreur lors du choix d'options
